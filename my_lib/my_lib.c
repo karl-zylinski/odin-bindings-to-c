43543300739351079f53c0db68437ce111a2a618
Karl Zylinski <karl@zylinski.se>
2024-09-18 02:05:33 +0200

    Update my_lib.c

--- my_lib/my_lib.c
+++ my_lib/my_lib.c
@@ -21,8 +21,12 @@ void do_stuff(TestStruct ts) {
 	ts.num += 1;
 	ts.flt_num -= 1;
 
-	// If messages come in the wrong order,
-	// the this will force flush the console buffer.
+	// This is is not important, but:
+	// This will force flush the console buffer,
+	// avoiding C and Odin prints to be out of order.
+	// This is not a good solution for this, instead
+	// you should probably redirect the C library's
+	// stdout to Odin's stdout.
 	fflush(stdout);
 	
 	if (callback != NULL) {
